222524035d234d5035f539c18e8d216b78d49bae
Hans Barredo <hansit.barredo@gmail.com>
2025-01-18 12:27:03 +0800

    js update

--- scripts/temples-large.js
+++ scripts/temples-large.js
@@ -1,21 +1,35 @@
+const mainnav = document.querySelector('.small');
+const hambutton = document.querySelector('#menu');
+const branding = document.querySelector('#navigation');
+
+hambutton.addEventListener('click', () => {
+  mainnav.classList.toggle('show');
+  hambutton.classList.toggle('show');
+  branding.classList.toggle('hide');
+});
+
 function checkScreenSize() {
-    const screenWidthInPixels = window.innerWidth;
-    const thresholdWidth = 40 * 16; 
-    const nav = document.querySelector('#navigation'); // Get the element to check for hide class
-  
-    if (screenWidthInPixels >= thresholdWidth) {
+  const screenWidthInPixels = window.innerWidth;
+  const thresholdWidth = 40 * 16;
+
+  const nav = document.querySelector('#navigation'); 
+
+  if (screenWidthInPixels >= thresholdWidth) {
+    if (nav.classList.contains('hide')) {
       nav.classList.remove('hide');
-    } else {
+    }
+  } else {
+    if (!nav.classList.contains('hide')) {
       nav.classList.add('hide');
     }
   }
-  
-  window.addEventListener('resize', checkScreenSize);
-  window.addEventListener('load', checkScreenSize);
-  
-  const modified = document.querySelector("#modified");
-  
-  let oLastModif = new Date(document.lastModified);
-  
-  modified.innerHTML = `Last Modification: <span class="last-mod">${oLastModif.toLocaleString()}</span>`;
-  
+}
+
+window.addEventListener('resize', checkScreenSize);
+window.addEventListener('load', checkScreenSize);
+
+const modified = document.querySelector("#modified");
+
+let oLastModif = new Date(document.lastModified);
+
+modified.innerHTML = `Last Modification: <span class="last-mod">${oLastModif.toLocaleString()}</span>`;
